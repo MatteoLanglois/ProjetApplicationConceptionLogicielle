c376c119a9089a2cfe99bf2c3b1bbaa18ce40866
matteolanglois <langlois.matteo49@gmail.com>
2023-12-23 11:05:40 +0100

    Added the bonus file with the documentation header.

---
+++ src/puissanceQuatre/bonus.py
@@ -0,0 +1,17 @@
+"""! @brief Un programme qui joue au jeu puissance 4++.
+
+@mainpage Projet Puissance 4++
+
+@section description_main Description
+Ce programme est un jeu de puissance 4++ avec une grille de taille variable,
+un nombre de pions à aligner variable, des bonus et un undo.
+
+@section import_section Importations
+Ce programme utilise les modules externes suivants :
+- tkinter
+- numpy
+
+@package src.puissanceQuatre.bonus
+@brief Ce module contient les fonctions relatives aux bonus.
+@details Ce module contient les fonctions de bonus.
+"""
